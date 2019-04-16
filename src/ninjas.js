4e9727b866220486de7cd067b01afcb4b85a1e35
Andy_Chou <andy770921>
2019-04-16 19:35:26 +0800

    replace stateless component from arrow function

--- src/ninjas.js
+++ src/ninjas.js
@@ -1,24 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Ninjas extends Component {
-    render(){
-        const Array = this.props.ninjasArray;
-        const ninjaList = Array.map( element => {
-            return (
-                <div className="ninja" key="Array.id">
-                    <div>Name: { element.name }</div>
-                    <div>Age: { element.age }</div>
-                    <div>Belt: { element.belt }</div>
-                </div>
-                );
-
-            });
+const Ninjas = (props) => {
+    const Array = props.ninjasArray;
+    const ninjaList = Array.map( element => {
         return (
+            <div className="ninja" key="Array.id">
+                <div>Name: { element.name }</div>
+                <div>Age: { element.age }</div>
+                <div>Belt: { element.belt }</div>
+            </div>
+            );
+        });
+    return (
         <div className = "ninjaList">
             { ninjaList }
         </div>)  
-    }
-
-}
+};
 
 export default Ninjas;

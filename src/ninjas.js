1c92d8a745e23b6ecd6b6ca7a72100347eccb91f
Andy_Chou <andy770921>
2019-04-16 19:28:57 +0800

    use state array rather than single props

--- src/ninjas.js
+++ src/ninjas.js
@@ -2,14 +2,21 @@ import React, { Component } from 'react';
 
 class Ninjas extends Component {
     render(){
-        const { name, age, belt } = this.props;
-        return(
-        <div className="ninja">
-            <div>Name: { name }</div>
-            <div>Age: { age }</div>
-            <div>Belt: { belt }</div>
-        </div>
-        );
+        const Array = this.props.ninjasArray;
+        const ninjaList = Array.map( element => {
+            return (
+                <div className="ninja" key="Array.id">
+                    <div>Name: { element.name }</div>
+                    <div>Age: { element.age }</div>
+                    <div>Belt: { element.belt }</div>
+                </div>
+                );
+
+            });
+        return (
+        <div className = "ninjaList">
+            { ninjaList }
+        </div>)  
     }
 
 }
